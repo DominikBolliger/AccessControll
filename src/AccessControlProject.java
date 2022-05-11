1053db1f1cd95054083d9b4f2849ec352795c4be
Nik181 <n_bolliger45@hotmail.com>
2022-05-11 13:18:38 +0200

    daf

--- src/AccessControlProject.java
+++ src/AccessControlProject.java
@@ -1,40 +1,29 @@
-import ch.abbts.model.Model;
-
 public class AccessControlProject {
     public static void main(String[] args) {
 
         TouchSensor ts1 = new TouchSensor();
         ts1.name = "Taste 1";
-        ts1.port = 3;
+        ts1.port = 1;
 
         TouchSensor ts2 = new TouchSensor();
         ts2.name = "Taste 2";
-        ts2.port = 4;
+        ts2.port = 2;
 
-        DistanzSensor ds1 = new DistanzSensor();
-        ds1.name = "Sensor 1";
-        ds1.port = 1;
+       DistanzSensor ds1 = new DistanzSensor();
+       ds1.name = "Distanz Sensor 1";
+       ds1.port = 3;
 
-        FarbSensor fs1 = new FarbSensor();
-        fs1.name = "Sensor 1";
-        fs1.port = 2;
+       Motor m1 = new Motor();
+       m1.name = "Motor 1";
+       m1.port = "A";
 
-        Motor mot1 = new Motor();
-        mot1.name = "Motor 1";
-        mot1.port = "A";
-
-        while (true){
-
-            while (!ts1.isPressed()){
-
-            }
-            mot1.openDor();
-
-            while (!ts2.isPressed()){
-
-            }
-            mot1.closeDor();
-
-        }
+       while(true){
+           if (ts1.isPressed()){
+               m1.openDor();
+           }
+           if (ts2.isPressed()){
+               m1.closeDor();
+           }
+       }
     }
 }
